c77a85cf01b274ce710897502522652450bce856
JakeRyanPlatt <jakeplatt176290@gmail.com>
2026-02-05 20:14:52 -0500

    Midterm start files, added icon and designed form

---
+++ Week5/MidTermAssignment/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace MidTermAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Fname = new System.Windows.Forms.Label();
+            this.txtFname = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Lname = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Mname = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Street1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Street2 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.City = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.State = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.ZIPCODE = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.PhoneNumber = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Fname
+            // 
+            this.Fname.AutoSize = true;
+            this.Fname.Location = new System.Drawing.Point(136, 62);
+            this.Fname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Fname.Name = "Fname";
+            this.Fname.Size = new System.Drawing.Size(0, 13);
+            this.Fname.TabIndex = 0;
+            // 
+            // txtFname
+            // 
+            this.txtFname.AutoSize = true;
+            this.txtFname.Location = new System.Drawing.Point(31, 29);
+            this.txtFname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(82, 13);
+            this.txtFname.TabIndex = 1;
+            this.txtFname.Text = "First Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Lname
+            // 
+            this.Lname.AutoSize = true;
+            this.Lname.Location = new System.Drawing.Point(29, 94);
+            this.Lname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lname.Name = "Lname";
+            this.Lname.Size = new System.Drawing.Size(79, 13);
+            this.Lname.TabIndex = 3;
+            this.Lname.Text = "Last Name:";
+            this.Lname.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 87);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(252, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Mname
+            // 
+            this.Mname.AutoSize = true;
+            this.Mname.Location = new System.Drawing.Point(31, 62);
+            this.Mname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Mname.Name = "Mname";
+            this.Mname.Size = new System.Drawing.Size(94, 13);
+            this.Mname.TabIndex = 5;
+            this.Mname.Text = "Middle Name:";
+            this.Mname.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(140, 59);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(252, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Street1
+            // 
+            this.Street1.AutoSize = true;
+            this.Street1.Location = new System.Drawing.Point(29, 126);
+            this.Street1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Street1.Name = "Street1";
+            this.Street1.Size = new System.Drawing.Size(59, 13);
+            this.Street1.TabIndex = 7;
+            this.Street1.Text = "Street1:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(140, 119);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(252, 21);
+            this.textBox4.TabIndex = 8;
+            // 
+            // Street2
+            // 
+            this.Street2.AutoSize = true;
+            this.Street2.Location = new System.Drawing.Point(29, 159);
+            this.Street2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Street2.Name = "Street2";
+            this.Street2.Size = new System.Drawing.Size(59, 13);
+            this.Street2.TabIndex = 9;
+            this.Street2.Text = "Street2:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(140, 156);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(252, 21);
+            this.textBox5.TabIndex = 10;
+            // 
+            // City
+            // 
+            this.City.AutoSize = true;
+            this.City.Location = new System.Drawing.Point(33, 193);
+            this.City.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(36, 13);
+            this.City.TabIndex = 11;
+            this.City.Text = "City:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(140, 190);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(252, 21);
+            this.textBox6.TabIndex = 12;
+            // 
+            // State
+            // 
+            this.State.AutoSize = true;
+            this.State.Location = new System.Drawing.Point(35, 227);
+            this.State.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.State.Name = "State";
+            this.State.Size = new System.Drawing.Size(45, 13);
+            this.State.TabIndex = 13;
+            this.State.Text = "State:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(140, 224);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(252, 21);
+            this.textBox7.TabIndex = 14;
+            // 
+            // ZIPCODE
+            // 
+            this.ZIPCODE.AutoSize = true;
+            this.ZIPCODE.Location = new System.Drawing.Point(35, 264);
+            this.ZIPCODE.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ZIPCODE.Name = "ZIPCODE";
+            this.ZIPCODE.Size = new System.Drawing.Size(67, 13);
+            this.ZIPCODE.TabIndex = 15;
+            this.ZIPCODE.Text = "Zip Code:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(140, 261);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(252, 21);
+            this.textBox8.TabIndex = 16;
+            // 
+            // PhoneNumber
+            // 
+            this.PhoneNumber.AutoSize = true;
+            this.PhoneNumber.Location = new System.Drawing.Point(35, 304);
+            this.PhoneNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PhoneNumber.Name = "PhoneNumber";
+            this.PhoneNumber.Size = new System.Drawing.Size(106, 13);
+            this.PhoneNumber.TabIndex = 17;
+            this.PhoneNumber.Text = "Phone Number:";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(152, 304);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(240, 21);
+            this.textBox9.TabIndex = 18;
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(35, 338);
+            this.Email.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(47, 13);
+            this.Email.TabIndex = 19;
+            this.Email.Text = "Email:";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(152, 338);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(240, 21);
+            this.textBox10.TabIndex = 20;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.btnAdd.Location = new System.Drawing.Point(585, 299);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(207, 59);
+            this.btnAdd.TabIndex = 21;
+            this.btnAdd.Text = "Submit!";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1067, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.PhoneNumber);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.ZIPCODE);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.State);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Street2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Street1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Mname);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Lname);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.Fname);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Contacts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Fname;
+        private System.Windows.Forms.Label txtFname;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Lname;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Mname;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label Street1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label Street2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label City;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label State;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label ZIPCODE;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label PhoneNumber;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Week5/MidTermAssignment/Program.cs
@@ -0,0 +1,150 @@
+﻿// Jake Platt
+// SE245
+// 2/4/2026
+// This program demonstrates a Person class with property-based validation
+
+using System;
+
+namespace Lab4_FormValid
+{
+    public class Person
+    {
+        // private fields
+        private string firstName = "";
+        private string middleName = "";
+        private string lastName = "";
+        private string street1 = "";
+        private string street2 = "";
+        private string city = "";
+        private string state = "";
+        private string zip = "";
+        private string phone = "";
+        private string email = "";
+
+        // public properties with validation
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (Validate.IsFilledIn(value))
+                    firstName = value;
+                else
+                    throw new Exception("First name is required.");
+            }
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+            set { middleName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (Validate.IsFilledIn(value))
+                    lastName = value;
+                else
+                    throw new Exception("Last name is required.");
+            }
+        }
+
+        public string Street1
+        {
+            get { return street1; }
+            set { street1 = value; }
+        }
+
+        public string Street2
+        {
+            get { return street2; }
+            set { street2 = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public string Zip
+        {
+            get { return zip; }
+            set
+            {
+                // at least 5 characters
+                if (Validate.IsMinLength(value, 5))
+                    zip = value;
+                else
+                    throw new Exception("ZIP must be at least 5 characters long.");
+            }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                // at least 10 characters
+                if (Validate.IsMinLength(value, 10))
+                    phone = value;
+                else
+                    throw new Exception("Phone must be at least 10 characters long.");
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (Validate.IsValidEmail(value))
+                    email = value;
+                else
+                    throw new Exception("Email is not in valid format.");
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            // These all go through the properties (validation)
+            p.FirstName = "John";
+            p.MiddleName = "Q";
+            p.LastName = "Public";
+            p.Street1 = "123 Main St";
+            p.Street2 = "Apt 4B";
+            p.City = "Somewhere";
+            p.State = "CA";
+            p.Zip = "90210";
+            p.Phone = "5551234567";
+            p.Email = "john.public@example.com";
+
+            // Attempted "hack" now must satisfy the FirstName setter
+            p.FirstName = p.FirstName + " - You've been hacked pal!";
+
+            Console.WriteLine("Person data stored:");
+            Console.WriteLine($"Name: {p.FirstName} {p.MiddleName} {p.LastName}");
+            Console.WriteLine($"Address: {p.Street1}, {p.Street2}");
+            Console.WriteLine($"{p.City}, {p.State} {p.Zip}");
+            Console.WriteLine($"Phone: {p.Phone}");
+            Console.WriteLine($"Email: {p.Email}");
+
+            Console.ReadLine();
+        }
+    }
+}
